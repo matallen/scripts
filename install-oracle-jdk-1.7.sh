8317292e466128946f60360369fe19b93bda85a9
Mat Allen <mallen@mallen.redhat>
2014-08-05 17:39:58 +0100

    initial

---
+++ install-oracle-jdk-1.7.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /opt/rh
+cd /opt/rh
+
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz
+gunzip jdk-7u67-linux-x64.tar.gz
+tar -xvf jdk-7u67-linux-x64.tar
+
+JDKS=`find . -maxdepth 1 -name "jdk*" -type d`;
+for folder in $JDKS
+do
+ ln -s $folder jdk_current
+ ln -s `pwd`/jdk_current/bin/java /bin/java
+done
---
+++ port-forward-8080-to-80.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+iptables -I INPUT 1 -p tcp --dport 8443 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
+iptables -L -n
