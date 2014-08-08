fd31d382608e26be25ee4388097369c63a346877
Mat Allen <mallen@mallen.redhat>
2014-08-08 09:58:35 +0100

    added oracle jdk 8

---
+++ install-oracle-jdk-1.8.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /opt/rh
+cd /opt/rh
+
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz
+gunzip jdk-8u11-linux-x64.tar.gz
+tar -xvf jdk-8u11-linux-x64.tar.gz
+
+JDKS=`find . -maxdepth 1 -name "jdk*" -type d`;
+for folder in $JDKS
+do
+ ln -s $folder jdk_current
+ ln -s `pwd`/jdk_current/bin/java /bin/java
+done
